e989fd55c3be9921eaa83bf47195dd8ce99a6ab7
MasterBel2 <39206414+MasterBel2@users.noreply.github.com>
2020-04-22 04:01:17 +1000

    Initial Commit

---
+++ SpringRTSLuaIDETest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SpringRTSLuaIDETest
+//
+//  Created by Derek Bel on 22/4/20.
+//  Copyright © 2020 MasterBel2. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
